f5d8dd5949e0d1017a25eb3f688d4da842d5af79
JO ARIMA <mante8251@yahoo.co.jp>
2024-06-22 22:59:21 +0900

    CORS

--- api/s3-upload.ts
+++ api/s3-upload.ts
@@ -4,27 +4,64 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 // cors setting for local development
 // see: https://github.com/vercel/vercel/issues/10927
-// const allowCors = (func) => {
-//   return (req, res) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader(
-//       'Access-Control-Allow-Methods',
-//       'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-//     )
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return func(req, res)
-//   }
-// }
+const allowCors = (func) => {
+  return (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader(
+      'Access-Control-Allow-Origin',
+      `${process.env.CORS_ALLOWED_ORIGIN}`
+    )
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+    )
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return func(req, res)
+  }
+}
 
-// const POST = allowCors(async (request, response) => {
+const POST = allowCors(
+  async (request: VercelRequest, response: VercelResponse) => {
+    const { filename, contentType } = await request.body
+
+    try {
+      const client = new S3Client({ region: process.env.AWS_S3_REGION })
+
+      if (!process.env.AWS_S3_BUCKET)
+        return response.json({
+          error: 'environment variable AWS_S3_BUCKET not found.',
+        })
+
+      const { url, fields } = await createPresignedPost(client, {
+        Bucket: process.env.AWS_S3_BUCKET,
+        Key: 'images/' + filename,
+        Conditions: [
+          ['content-length-range', 0, 104857600],
+          ['starts-with', '$Content-Type', contentType],
+        ],
+        Fields: {
+          acl: 'public-read',
+          'Content-Type': contentType,
+        },
+        Expires: 600, // Seconds before the presigned post expires. 3600 by default.
+      })
+
+      return response.json({ url, fields })
+    } catch (error) {
+      console.log(error)
+      return response.json({ error: 'createPresignedPost error.' })
+    }
+  }
+)
+
+// const POST = async (request: VercelRequest, response: VercelResponse) => {
 //   const { filename, contentType } = await request.body
 
 //   try {
@@ -51,39 +88,8 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 //     return response.json({ url, fields })
 //   } catch (error) {
-//     return response.json({ error: error.message })
+//     console.log(error)
+//     return response.json({ error: 'createPresignedPost error.' })
 //   }
-// })
-
-const POST = async (request: VercelRequest, response: VercelResponse) => {
-  const { filename, contentType } = await request.body
-
-  try {
-    const client = new S3Client({ region: process.env.AWS_S3_REGION })
-
-    if (!process.env.AWS_S3_BUCKET)
-      return response.json({
-        error: 'environment variable AWS_S3_BUCKET not found.',
-      })
-
-    const { url, fields } = await createPresignedPost(client, {
-      Bucket: process.env.AWS_S3_BUCKET,
-      Key: 'images/' + filename,
-      Conditions: [
-        ['content-length-range', 0, 104857600],
-        ['starts-with', '$Content-Type', contentType],
-      ],
-      Fields: {
-        acl: 'public-read',
-        'Content-Type': contentType,
-      },
-      Expires: 600, // Seconds before the presigned post expires. 3600 by default.
-    })
-
-    return response.json({ url, fields })
-  } catch (error) {
-    console.log(error)
-    return response.json({ error: 'createPresignedPost error.' })
-  }
-}
+// }
 export default POST
